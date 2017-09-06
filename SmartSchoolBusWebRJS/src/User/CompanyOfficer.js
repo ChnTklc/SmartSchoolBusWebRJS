7849a07a43db4013e2f3c4029a2dd1083a0b4f3e
Cihan <cihan@tklc>
2017-09-06 17:08:42 +0300

    Table added to company officer page.

--- SmartSchoolBusWebRJS/src/User/CompanyOfficer.js
+++ SmartSchoolBusWebRJS/src/User/CompanyOfficer.js
@@ -10,6 +10,8 @@ import ArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
 import { List, ListItem } from 'material-ui/List';
 import Login from './Login';
 import NavigationBar from '../Home/NavigationBar';
+import { Table, TableRow, TableRowColumn, TableBody, TableHeader, TableHeaderColumn } from 'material-ui/Table';
+import FlatButton from 'material-ui/FlatButton';
 
 let varsAsLanguage = {
     en: {
@@ -25,6 +27,32 @@ let varsAsLanguage = {
 };
 
 var language = varsAsLanguage.en;
+
+var Students = []
+
+function addStudent(photo, name, surname, classNo, studentNo, parent, homeAdress, service) {
+    Students.push({
+        photo: photo,
+        studentNo: studentNo,
+        name: name,
+        surname: surname,
+        classNo: classNo,
+        parent: parent,
+        homeAdress: homeAdress,
+        service: service,
+    });
+    return Students;
+}
+for (var i = 1; i < 26; i++) {
+    Students = addStudent("http://studentreasures.com/wp-content/themes/kingpower-v1-08/images/profile_default.jpg",
+        "Cihan",
+        "Toklucu",
+        "4",
+        "210201027",
+        "Ali Veli",
+        "Gulbahce Mahallesi IYTE Kampusu A8 Binasi No:1 / 37 D:18 Urla / Izmir / Turkey",
+        "Service1");
+}
 
 const SchoolNames = [
     "İYTE",
@@ -46,7 +74,7 @@ class CompanyOfficer extends React.Component {
         super(props);
         this.state = {
             isLogin: true,
-            isDrawerOpen: false
+            isDrawerOpen: true
         }
     }
 
@@ -62,10 +90,6 @@ class CompanyOfficer extends React.Component {
     handleMenuToggle() {
         this.setState({ isDrawerOpen: !this.state.isDrawerOpen });
     };
-
-    handleClose() {
-        this.setState({ isDrawerOpen: false });
-    }
 
     logoutClick() {
         var self = this;
@@ -101,6 +125,9 @@ class CompanyOfficer extends React.Component {
     }
 
     showStudents(id) {
+        /*fix me*/
+        var itemStudents = document.getElementById(id);
+        itemStudents.setAttribute('color', 'grey');
         return true;
     }
 
@@ -119,7 +146,12 @@ class CompanyOfficer extends React.Component {
     showRoute(id) {
         return true;
     }
+    /*
+    close drawer button
 
+                            <FlatButton label="X" labelStyle={{ fontSize: 15, padding: 0 }} onClick={() => this.handleMenuToggle()}
+                                style={{ minWidth: 40, minHeight: 38, width: 40, height: 38, top: "2%", left: "2%", position: "absolute", border: 2, textAlign: "center", bottom: "8%" }} />
+    */
     render() {
         this.languageSetting();
         /*if (localStorage.getItem('isLoggedInCompanyOfficer') === 'false') {
@@ -127,14 +159,13 @@ class CompanyOfficer extends React.Component {
         }*/
         return (
             <MuiThemeProvider>
-                <div>
+                <div style={{ left: "18%", position: "absolute" }}>
                     <AppBar
                         className="appbar" style={{ backgroundColor: "rgba(61, 59, 59, 1)" }}
                         title={<a style={{ textDecoration: "none", cursor: "pointer", color: "white" }} href="/companyofficer">{language.title}</a>}
                         iconElementRight={<Button label={language.logout} style={{ margin: 0 }} labelStyle={{ fontSize: 18 }} onClick={(e) => this.logoutClick(e)} />}
                         onLeftIconButtonTouchTap={() => this.handleMenuToggle()} />
                     <Drawer
-                        docked={false}
                         width={250}
                         open={this.state.isDrawerOpen}
                         onRequestChange={(open) => this.setState({ isDrawerOpen: open })}>
@@ -155,7 +186,47 @@ class CompanyOfficer extends React.Component {
                             <Divider />
                         </List>
                     </Drawer>
-
+                    <div className="studentTableDiv">
+                        <Table
+                            height="540"
+                            fixedHeader={false}
+                            fixedFooter={false}
+                            selectable={false}>
+                            <TableHeader
+                                adjustForCheckbox={false}
+                                displaySelectAll={false}>
+                                <TableRow>
+                                    <TableHeaderColumn tooltip="Number">No</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Photo">Photo</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's School Number">Student No</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Name">Name</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Surname">Surname</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Class">Class</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Parent">Parent</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Adress">Adress</TableHeaderColumn>
+                                    <TableHeaderColumn tooltip="Student's Service">Service</TableHeaderColumn>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody
+                                displayRowCheckbox={false}
+                                showRowHover={false}
+                                stripedRows={true}>
+                                {Students.map((row, index) => (
+                                    <TableRow key={index + 1} displayBorder={true}>
+                                        <TableRowColumn>{index + 1}</TableRowColumn>
+                                        <TableRowColumn><img src={row.photo} style={{ width: 25, height: 25 }} /></TableRowColumn>
+                                        <TableRowColumn>{row.studentNo}</TableRowColumn>
+                                        <TableRowColumn>{row.name}</TableRowColumn>
+                                        <TableRowColumn>{row.surname}</TableRowColumn>
+                                        <TableRowColumn>{row.classNo}</TableRowColumn>
+                                        <TableRowColumn>{row.parent}</TableRowColumn>
+                                        <TableRowColumn>{row.homeAdress}</TableRowColumn>
+                                        <TableRowColumn>{row.service}</TableRowColumn>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </div>
                 </div>
             </MuiThemeProvider>
         );
